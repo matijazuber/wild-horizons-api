b00cb89e4f0c8a859a25440df690529d4935307e
Matija Zuber <matijazuber273@gmail.com>
2026-03-27 14:02:43 +0100

    converted the type from coomonjs to module, and added the data.js array of destinations, created the server using hhtp createServer, and api route to display a message

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Hello Node!");
+import http from "node:http";
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  // End send data over http and ends a response
+  if (req.url === "/api" && req.method === "GET") {
+    res.end("This is some data \n");
+  } else {
+    res.end("Not found");
+  }
+});
+
+server.listen(PORT, () => console.log(`Server running on port:${PORT}`));
